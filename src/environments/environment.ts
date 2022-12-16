1b6629b5423657ea3a50997c6dee12dbb52312e9
rohitCodeRed <rohit06051993@gmail.com>
2022-12-16 23:40:37 +0530

    add correct port

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  serverUrl: "http://localhost:5000"//"https://8424-2409-4063-6e93-88ce-d49e-e1c-73db-5be5.ap.ngrok.io" //"http://192.168.8.101:5000" //"http://localhost:5000"
+  serverUrl: "http://localhost:4000"//"https://8424-2409-4063-6e93-88ce-d49e-e1c-73db-5be5.ap.ngrok.io" //"http://192.168.8.101:5000" //"http://localhost:5000"
 };
 
 /*
